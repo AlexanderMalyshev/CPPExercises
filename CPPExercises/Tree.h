1b014c39cfb7c6aef9f8f0af89cd6d4b9cb492ec
Aleksandr Malyshev <almaly@microsoft.com>
2017-10-03 21:08:48 -0700

    Revert "Revert "Revert "+ConnectLayers into list"""
    This reverts commit dbe593fed733c681e89bb33c15454d7dcbdaf8e0.

--- CPPExercises/Tree.h
+++ CPPExercises/Tree.h
@@ -4,25 +4,39 @@
 
 class Node
 {
-	int m_data;
-	Node* m_ptr;
-	std::unique_ptr<Node> m_left;
-	std::unique_ptr<Node> m_right;
+	int data;
+	std::unique_ptr<Node> left;
+	std::unique_ptr<Node> right;
 
 public:
-	Node(int _data, Node* _left, Node* _right) : 
-		m_data { _data }, m_left { _left }, m_right { _right }
+	Node(int _data, Node* _left, Node* _right) : data { _data }, left { _left }, right { _right }
 	{
-		m_ptr = nullptr;
 	}
 
-	void AddLeft(Node* _left) {	m_left.reset(_left); }
-	void AddRight(Node* _right)	{ m_right.reset(_right); }
-	void SetPtr(Node* ptr)	{ m_ptr = ptr; }
-	int GetData() const	{ return m_data; }
-	Node* GetLeft() const {	return m_left.get(); }
-	Node* GetRight() const { return m_right.get(); }	
-	Node* GetPtr() const { return m_ptr; }
+	void AddLeft(Node* _left)
+	{
+		left.reset(_left);
+	}
+
+	void AddRight(Node* _right)
+	{
+		right.reset(_right);
+	}
+
+	int GetData() const
+	{
+		return data;
+	}
+
+	Node* GetLeft() const
+	{
+		return left.get();
+	}
+
+	Node* GetRight() const
+	{
+		return right.get();
+	}
 };
 
 class MyTree
@@ -39,6 +53,4 @@ public:
 	int GetHeight();
 
 	void PrintTree();
-
-	void ConnectLayers();
 };
